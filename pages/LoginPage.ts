95a17722f131db387aed4b1003518cf7a57f5cff
Affra Nazir <affranazir123@gmail.com>
2026-08-06 16:43:37 +0500

    feat(login): implement login flow using SauceDemo

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -2,23 +2,19 @@ import {Page,Locator} from '@playwright/test'
 
 export class LoginPage{
 
-    readonly loginButton:Locator;
     readonly usernameInput:Locator;
     readonly passwordInput:Locator;
     readonly submitLoginButton:Locator;
 
     constructor (private page: Page){
-        this.loginButton=this.page.getByRole('button',{name:'Login'});
-        this.usernameInput=this.page.getByPlaceholder('Username');
-        this.passwordInput=this.page.getByPlaceholder('Password');
-        this.submitLoginButton=this.page.locator("form").getByRole('button',{name:'Login'});
+
+    
+        this.usernameInput=this.page.getByPlaceholder('Username')
+        this.passwordInput=this.page.getByPlaceholder('Password')
+        this.submitLoginButton=this.page.locator('.submit-button');
     }
 
 
-
-    async clickLoginButton(){
-        await this.loginButton.click();
-    }
 
     async enterUsername(username:string){
         await this.usernameInput.fill(username);
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,15 +3,15 @@ import { LoginPage } from '../pages/LoginPage';
 
 test("User should open Login Popup",async ({page})=>{
 
-    await page.goto("https://bookcart.azurewebsites.net/");
+    await page.goto("https://www.saucedemo.com/");
 
     const loginPage = new LoginPage(page);
 
-    await loginPage.clickLoginButton();
-
-    await loginPage.enterUsername("testuser");
-    await loginPage.enterPassword("Test123");
+    await loginPage.enterUsername("standard_user");
+    await loginPage.enterPassword("secret_sauce");
     await loginPage.clickSubmitLoginButton();
+
+    await page.waitForTimeout(5000);
 
 })
 
