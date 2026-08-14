62e636a6d9e8d26887fb6eaaa99bf2b5bf550b62
Affra Nazir <affranazir123@gmail.com>
2026-08-14 20:30:11 +0500

    feat(test-fixtures: add reusable login fixtures

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,88 +1,56 @@
-import {test} from "@playwright/test";
+import {test as base} from "@playwright/test";
 import {LoginPage} from "../pages/LoginPage";
 import { InventoryPage } from "../pages/InventoryPage";
 
-test("User should see Inventory page after login", async ({page})=>{
+const test = base.extend<{
+    loginPage: LoginPage;
+}>({
+    loginPage: async ({ page }, use) => {
+        await page.goto("https://www.saucedemo.com/");
 
-    await page.goto("https://www.saucedemo.com/");
+        const loginPage = new LoginPage(page);
 
+        await loginPage.enterUsername("standard_user");
+        await loginPage.enterPassword("secret_sauce");
+        await loginPage.clickSubmitLoginButton();
+
+        await use(loginPage);
+    },
+});
+
+test("User should see Inventory page after login", async ({page,loginPage})=>{
 
     const inventoryPage= new InventoryPage(page);
-    const loginPage = new LoginPage(page);
-
-
-
-    await loginPage.enterUsername("standard_user");
-    await loginPage.enterPassword("secret_sauce");
-    await loginPage.clickSubmitLoginButton();
-
-
     await inventoryPage.verifyInventoryPageLoaded();
 
 });
 
-test("User should be able to sort products by price low to high", async ({page})=>{
+test("User should be able to sort products by price low to high", async ({page,loginPage})=>{
 
-
-    await page.goto("https://www.saucedemo.com/");
-
-    const loginPage = new LoginPage(page);
     const inventoryPage = new InventoryPage(page);
-
-
-    await loginPage.enterUsername("standard_user");
-    await loginPage.enterPassword("secret_sauce");
-    await loginPage.clickSubmitLoginButton();
-
     await inventoryPage.sortProductByPriceLowToHigh();
 
     await inventoryPage.verifyProductsSortedByPriceLowToHigh();
     
 });
 
-test("User should be able to sort product by name Z to A", async ({page})=>{
-    await page.goto("https://www.saucedemo.com/");
+test("User should be able to sort product by name Z to A", async ({page,loginPage})=>{
 
-    const loginPage = new LoginPage(page);
     const inventoryPage = new InventoryPage(page);
-
-
-    await loginPage.enterUsername("standard_user");
-    await loginPage.enterPassword("secret_sauce");
-    await loginPage.clickSubmitLoginButton();
-
     await inventoryPage.sortPorductByNameZToA();
 
 });
 
-test("User should be able to sort product by name A to Z", async ({page})=>{
-    await page.goto("https://www.saucedemo.com/");
+test("User should be able to sort product by name A to Z", async ({page,loginPage})=>{
 
-    const loginPage = new LoginPage(page);
     const inventoryPage = new InventoryPage(page);
-
-
-    await loginPage.enterUsername("standard_user");
-    await loginPage.enterPassword("secret_sauce");
-    await loginPage.clickSubmitLoginButton();
-
     await inventoryPage.sortProductByNameAToZ();
 
 });
 
-test("User should be able to sort products by price high to low", async ({page})=>{
+test("User should be able to sort products by price high to low", async ({page,loginPage})=>{
 
-
-    await page.goto("https://www.saucedemo.com/");
-
-    const loginPage = new LoginPage(page);
     const inventoryPage = new InventoryPage(page);
-
-
-    await loginPage.enterUsername("standard_user");
-    await loginPage.enterPassword("secret_sauce");
-    await loginPage.clickSubmitLoginButton();
-
     await inventoryPage.sortPorductByPriceHighToLow();
 
     await inventoryPage.verifyProductSortedByPriceHighToLow();
