f908f2fde12afc6d1bb7a83836b55eac33072159
Affra Nazir <affranazir123@gmail.com>
2026-08-14 10:59:57 +0500

    feat(inventory): add product sorting tests

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,4 +1,4 @@
-import {test,expect} from "@playwright/test";
+import {test} from "@playwright/test";
 import {LoginPage} from "../pages/LoginPage";
 import { InventoryPage } from "../pages/InventoryPage";
 
@@ -11,6 +11,7 @@ test("User should see Inventory page after login", async ({page})=>{
     const loginPage = new LoginPage(page);
 
 
+
     await loginPage.enterUsername("standard_user");
     await loginPage.enterPassword("secret_sauce");
     await loginPage.clickSubmitLoginButton();
@@ -18,4 +19,73 @@ test("User should see Inventory page after login", async ({page})=>{
 
     await inventoryPage.verifyInventoryPageLoaded();
 
+});
+
+test("User should be able to sort products by price low to high", async ({page})=>{
+
+
+    await page.goto("https://www.saucedemo.com/");
+
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+
+
+    await loginPage.enterUsername("standard_user");
+    await loginPage.enterPassword("secret_sauce");
+    await loginPage.clickSubmitLoginButton();
+
+    await inventoryPage.sortProductByPriceLowToHigh();
+
+    await inventoryPage.verifyProductsSortedByPriceLowToHigh();
+    
+});
+
+test("User should be able to sort product by name Z to A", async ({page})=>{
+    await page.goto("https://www.saucedemo.com/");
+
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+
+
+    await loginPage.enterUsername("standard_user");
+    await loginPage.enterPassword("secret_sauce");
+    await loginPage.clickSubmitLoginButton();
+
+    await inventoryPage.sortPorductByNameZToA();
+
+});
+
+test("User should be able to sort product by name A to Z", async ({page})=>{
+    await page.goto("https://www.saucedemo.com/");
+
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+
+
+    await loginPage.enterUsername("standard_user");
+    await loginPage.enterPassword("secret_sauce");
+    await loginPage.clickSubmitLoginButton();
+
+    await inventoryPage.sortProductByNameAToZ();
+
+});
+
+test("User should be able to sort products by price high to low", async ({page})=>{
+
+
+    await page.goto("https://www.saucedemo.com/");
+
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+
+
+    await loginPage.enterUsername("standard_user");
+    await loginPage.enterPassword("secret_sauce");
+    await loginPage.clickSubmitLoginButton();
+
+    await inventoryPage.sortPorductByPriceHighToLow();
+
+    await inventoryPage.verifyProductSortedByPriceHighToLow();
+
+    
 });
